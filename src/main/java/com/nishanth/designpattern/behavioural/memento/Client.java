4e3a31119fa591cfdc1bd7a3933bca127d28acab
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-07-30 23:54:43 +0530

    behavioural - memento - usecase [for undo machanism]

---
+++ src/main/java/com/nishanth/designpattern/behavioural/memento/Client.java
@@ -0,0 +1,14 @@
+package com.nishanth.designpattern.behavioural.memento;
+
+// Memento pattern used for undo mechanism
+public class Client {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.setContent("a");
+        editor.setContent("b");
+        editor.setContent("c");
+
+        // Need to write logic for undo
+        // editor.undo();
+    }
+}
