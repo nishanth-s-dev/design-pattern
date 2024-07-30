f68848af6a96697031f1b24e66e5f782a3c81057
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-07-31 03:41:50 +0530

    behavioural - strategy - sidenote

--- src/main/java/com/nishanth/designpattern/behavioural/strategy/Client.java
+++ src/main/java/com/nishanth/designpattern/behavioural/strategy/Client.java
@@ -5,6 +5,14 @@ import com.nishanth.designpattern.behavioural.strategy.filter.ImageFilter;
 import com.nishanth.designpattern.behavioural.strategy.compressor.JPEGCompressor;
 import com.nishanth.designpattern.behavioural.strategy.filter.BlackAndWhiteImageFilter;
 
+
+/*
+    * Difference between state pattern and strategy pattern.
+    * In state pattern the class behaves based on the current state object, which is currentTool in our example.
+    * The currentTool is a concrete implementation of Tool interface. which has two methods in our case. all tools should have that methods.
+    * In state pattern there is only one state, in contrast strategy have more than one. Different behaviour are represented based on different strategy object.
+ */
+
 public class Client {
     public static void main(String[] args) {
         ImageFilter imageFilter = new BlackAndWhiteImageFilter();
