0b6cb451f68a588b74bea4e65276057a35f72350
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-08-02 02:56:01 +0530

    creational - singleton - solution

--- src/main/java/com/nishanth/designpattern/creational/singleton/ConfigManager.java
+++ src/main/java/com/nishanth/designpattern/creational/singleton/ConfigManager.java
@@ -3,8 +3,23 @@ package com.nishanth.designpattern.creational.singleton;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ConfigManager {
-    private Map<String, Object> settings = new HashMap<String, Object>();
+public final class ConfigManager {
+    private static volatile ConfigManager configManager;
+
+    private ConfigManager() {}
+
+    public static ConfigManager getInstance() {
+        if (configManager == null) {
+            synchronized (ConfigManager.class) {
+                if (configManager == null) {
+                    return new ConfigManager();
+                }
+            }
+        }
+        return configManager;
+    }
+
+    private final Map<String, Object> settings = new HashMap<String, Object>();
 
     public void addSetting(String key, Object value) {
         settings.put(key, value);
